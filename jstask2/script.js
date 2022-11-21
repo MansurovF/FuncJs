03a64d155ddd3782d389c59761377a7c0582e470
MansurovF <112488802+MansurovF@users.noreply.github.com>
2022-11-21 20:23:47 +0400

    Add files via upload

---
+++ jstask2/script.js
@@ -0,0 +1,47 @@
+//Task2
+// function myFunc(){
+//     let People=prompt("Please enter the age");
+//     if(People>=18){
+//         document.getElementById("People").innerHTML="Xos Gelmisiniz <br> Benvenuti in questa terra";
+//     }
+//     else {
+//         document.getElementById("People").innerHTML="Yaşınız kifayət qədər deyil";
+//     }
+// }
+
+//Task1
+// function myFunc(){
+//     let text="Click the OK!";
+//     if (confirm(text) == true) {
+//         text = "Welcome the website";
+//       } else {
+//         text = "You canceled, try again!";
+//       }
+//       document.getElementById("User").innerHTML=text;
+    
+// }
+
+//Task3
+// function myFunc(){
+//     var box = document.getElementById('textbox');
+//     var no = box.value.slice(-3, -2);
+//     if(no == 2){
+//         document.getElementById('User').style.color="Orange"
+//         document.getElementById('textbox').style.color="Orange"
+//         text  ="Gunorta qrupu";
+//     }
+//     else if(no==3){
+//         document.getElementById('User').style.color="black"
+//         document.getElementById('textbox').style.color="black"
+
+//         text="Axsam qrupu";
+//     }
+//     else{
+//         document.getElementById('User').style.color="yellow"
+//         document.getElementById('textbox').style.color="yellow"
+
+//         text="Seher qrupu";
+//     }
+//         document.getElementById("User").innerHTML=text;
+
+// }
